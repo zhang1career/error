a4266479d8cd8af19146102e05549e71c5fc25ab
曾经瘦过 <zhang_career@foxmail.com>
2017-09-10 10:41:20 +0800

    param min/max added

--- src/Base.php
+++ src/Base.php
@@ -12,5 +12,8 @@ namespace phplab\error;
 class Base
 {
     const BASE_OK                   = 0;
-    const BASE_PARAM_NOT_EXISTS     = 1;
+
+    const BASE_PARAM_NOT_EXISTS     = 10;
+    const BASE_PARAM_UNDERFLOW      = 11;
+    const BASE_PARAM_OVERFLOW       = 12;
 }
--- src/system/System.php
+++ src/system/System.php
@@ -12,6 +12,17 @@ use phplab\error\Base;
 
 class System extends Base
 {
-    const ERROR_OK                  = self::BASE_OK;
-    const ERROR_PARAM_NOT_EXISTS    = self::BASE_PARAM_NOT_EXISTS;
+    const ERROR_OK = self::BASE_OK;
+
+    const ERROR_PARAM_NOT_EXISTS = self::BASE_PARAM_NOT_EXISTS;
+    const ERROR_PARAM_UNDERFLOW = self::BASE_PARAM_UNDERFLOW;
+    const ERROR_PARAM_OVERFLOW = self::BASE_PARAM_OVERFLOW;
+
+
+    public static $MESSAGE = [
+        self::ERROR_OK               => 'OK',
+        self::ERROR_PARAM_NOT_EXISTS => 'Param not exists',
+        self::ERROR_PARAM_UNDERFLOW  => 'Param values too small',
+        self::ERROR_PARAM_OVERFLOW   => 'Param values too big',
+    ];
 }
